2b6efaf533703249ea2bfd06a41ebc3caf43f4f0
Konkhmer <lenicolegabriel@gmail.com>
2026-05-29 20:47:39 +0700

    Update history.js

--- js/history.js
+++ js/history.js
@@ -137,7 +137,7 @@ html += `
 		let rect = this.getBoundingClientRect();
 	
 		let left = rect.left + rect.width / 2;		
-		let top = rect.top - (tooltip.outerHeight() + 5) ;
+		let top = rect.top - (tooltip.outerHeight() + 15) ;
 	
 		if (top < 10) {
 			top = rect.bottom + 10;
